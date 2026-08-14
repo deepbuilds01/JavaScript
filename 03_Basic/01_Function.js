79b6a00e7a06af1bb4b65052fcfe266c86618912
Dk2004k-cc <deepkr556@gmail.com>
2026-08-15 01:38:23 +0530

    function

--- 03_Basic/01_Function.js
+++ 03_Basic/01_Function.js
@@ -14,10 +14,33 @@ function name(){
 function twosum(num1, num2){
     console.log(num1+num2);
 }
-twosum()      // funtion call output is NaN
-twosum(10,10) // 20
-twosum(10,"132") // 10132
-twosum(10,null)  // 10
-const result = twosum(50,50)
-console.log(result);    // undefined
+// twosum()      // funtion call output is NaN
+// twosum(10,10) // 20
+// twosum(10,"132") // 10132
+// twosum(10,null)  // 10
+// const result = twosum(50,50)
+// console.log(result);    // undefined becouse the function are print the sum not return
+
+function multi(num1, num2){
+    let ans = num1*num2
+    return ans
+}
+// console.log(multi(3,3));
+const result1 = multi(3,10)
+// console.log(result1);
+
+
+//+++++++++ basic if else
+function user(username){                   
+    if(username === undefined){
+        console.log("pls enter the username");
+        return
+    }
+    return  `${username} justlogged in`
+}
+console.log(user()); // if you can not pass the argument the output is "undefine"
+
+
+
+
 
