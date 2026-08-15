9e4d574e9d562640dca568bfd45ccdde1c50de56
Dk2004k-cc <deepkr556@gmail.com>
2026-08-15 18:48:48 +0530

    IIFE

---
+++ 03_Basic/04_IIFE.js
@@ -0,0 +1,22 @@
+// // Normal function declaration
+
+// function sum(num1, num2){
+//     console.log(num1+num2);
+// }
+// sum(5,23);
+
+// apply IIFE 
+// (function product(num1, num2){
+//     console.log(num1*num2);
+// })(5,25);
+
+// arrow function
+const name = 
+//++++++++++ OR
+(() => console.log("deep kumar"))();
+
+
+
+((name) => 
+console.log(name)
+)("DEEP KUMAR");
