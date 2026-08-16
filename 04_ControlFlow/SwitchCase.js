d7c0b0325e7674fb24f4dda3736dca29cedab220
Dk2004k-cc <deepkr556@gmail.com>
2026-08-16 13:02:52 +0530

    Switch case

---
+++ 04_ControlFlow/SwitchCase.js
@@ -0,0 +1,54 @@
+// Form,ate of Switch case
+
+// switch(Conditional){
+//     case 1 :
+//         break;
+//     default :
+//         break;
+// }
+
+
+// const month = 5       // may
+const month =78
+switch(month){
+    case 1 :{
+        console.log("january");
+        break;
+    }case 2 :{
+        console.log("feb");
+        break;
+    }case 3 :{
+        console.log("mar");
+        break;
+    }case 4 :{
+        console.log("april");
+        break;
+    }case 5 :{
+        console.log("may");
+        break;
+    }case 6 :{
+        console.log("june");
+        break;
+    }case 7 :{
+        console.log("july");
+        break;
+    }case 8 :{
+        console.log("August");
+        break;
+    }case 9 :{
+        console.log("sep");
+        break;
+    }case 10 :{
+        console.log("octuber");
+        break;
+    }case 11 :{
+        console.log("November");
+        break;
+    }case 12 :{
+        console.log("December");
+        break;
+    }default:{
+        console.log("Invelid number");
+        break; 
+    }
+}
